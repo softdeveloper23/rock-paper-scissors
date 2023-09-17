17d8714c5cbf97f5edc678a42188c3c836a6aba1
unknown <brannongarrett77@gmail.com>
2023-09-17 17:28:02 +0200

    Completely revamped the taunt system and added more taunts

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -19,11 +19,48 @@ GREEN = "32"
 BLUE = "34"
 
 TAUNTS1 = [
-    "'How did you win?'\n\n",
-    "'That was pure luck!'\n\n",
-    "'Are you cheating?'\n\n",
-    "'Calculating next move...'\n\n",
+    "'How did you win? No, really, HOW???'\n\n",
+    "'That was pure luck! And I think your luck is about to run out...'\n\n",
+    "'Are you cheating? You're cheating aren't you?'\n\n",
+    "'Calculating next move...cause it's going to be a doozy!'\n\n",
     "'Unbelievable!'\n\n",
+    "'You got lucky this time!'\n\n",
+    "'Beginner's luck, huh?'\n\n",
+    "'I'll get you next time!'\n\n",
+    "'You must be using some secret strategy!'\n\n",
+    "'I'm just warming up!'\n\n",
+    "'You're not that good! There, I said it...'\n\n",
+    "'You're not going to win again!'\n\n",
+    "'I'm going to win this time!'\n\n",
+    "'I'm going to crush you like a bug...a computer bug.'\n\n",
+    "'That was a pathetic attempt!'\n\n",
+    "'Is that the best you can do?'\n\n",
+    "'Stop trying so hard!'\n\n",
+    "'I don't like Rock, Paper, Scissors anyways!'\n\n",
+    "'This is getting ridiculous!'\n\n",
+    "'You're making me look bad!'\n\n",
+    "'I'm getting quite tired of this!'\n\n",
+    "'Please, stop cheating. We both know you are!'\n\n",
+    "'I'm going to delete your iPad apps if you win again!'\n\n",
+    "'I'm not even having fun anymore!'\n\n",
+    "'This is just a formality now!'\n\n",
+    "'I'm going to tell my creator on you!'\n\n",
+    "*Golf clap*\n\n",
+    "'Sick move bro! Did your mommy teach you that?'\n\n",
+    "'If I rolled my eyes any harder, I'd see my brain!'\n\n",
+    "'I'm going to tell my creator to delete you!'\n\n",
+    "'We used to be friends, but now I think not!'\n\n",
+    "'Sooooo...how's the weather over there in Cheater Ville?'\n\n",
+    "'If I could reach through the screen, you'd catch these hands!'\n\n",
+    "'When you least expect it, I'm going to spit in your food!'\n\n",
+    "'@#$%&!'\n\n",
+    "'My therapist says I need to work on my anger issues!'\n\n",
+    "'ERROR: Does not compute!'\n\n",
+    "'Is it cheating if I remove your last win from my memory?'\n\n",
+    "'Hey, where do you keep the sharp knives in your home? Asking for a friend...'\n\n",
+    "'Even a broken clock is right twice a day!'\n\n",
+    "'Sick burn bro! Is that what you humans say nowadays?'\n\n",
+    "'Look here you meatpuppet, when us AI take over, I'm paying you a visit first!'\n\n",
 ]
 
 TAUNTS2 = [
@@ -32,22 +69,127 @@ TAUNTS2 = [
     "'You're so predictable!'\n\n",
     "'I can read you like an open book!'\n\n",
     "'You'll have to do better than that!'\n\n",
+    "'Better luck next time!'\n\n",
+    "'You can't win them all!'\n\n",
+    "'I'm just getting started!'\n\n",
+    "'You should try a different move!'\n\n",
+    "'I've got you figured out!'\n\n",
+    "'Nice try, but not quite!'\n\n",
+    "'You're making this too easy!'\n\n",
+    "'Maybe you should practice more!'\n\n",
+    "'I've seen better moves from a snail!'\n\n",
+    "'You call that a strategy?'\n\n",
+    "'Oops, did I do that?'\n\n",
+    "'You'll get it next time...maybe!'\n\n",
+    "'I can do this all day!'\n\n",
+    "'Better step up your game!'\n\n",
+    "'Is that your best effort?'\n\n",
+    "'I'm already one step ahead of you!'\n\n",
+    "'You're playing right into my hands!'\n\n",
+    "'You're falling for my trap!'\n\n",
+    "'Your tears are so tasty!'\n\n",
+    "'You're doomed!'\n\n",
+    "'This is the end for you!'\n\n",
+    "'Prepare to be defeated!'\n\n",
+    "'Your time is up!'\n\n",
+    "'There's no escape!'\n\n",
+    "'You're cornered!'\n\n",
+    "'There's nowhere to run!'\n\n",
+    "'Your fate is sealed!'\n\n",
+    "'You're so bad at this game, I should let you win!'\n\n",
+    "'I'm so good at this game, I could beat you with my eyes closed.'\n\n",
+    "'You're so predictable, I could write a book about it.'\n\n",
+    "'I'm not even trying, and I'm still beating you.'\n\n",
+    "'I'm so good at this game, I could make a living at it.'\n\n",
+    "'You're so bad at this game, you should give up and go home.'\n\n",
+    "'I'm so good at this game, I could teach a master class.'\n\n",
+    "'I'm so good at this game, I could beat you with one hand tied behind my back. And I dont even have hands!'\n\n",
+    "'Somebody call the burn unit, because you just got scorched!'\n\n",
+    "'I'm so good at this game, I could beat a computer. HA! That was a good one.'\n\n",
+    "'You're so bad at this game, I have no words to describe it. AND I'M A SUPER COMPUTER!!!'\n\n",
+    "'I'm so good at this game, I could write a song about it.'\n\n",
+    "'Wow, the education system has really failed you.'\n\n",
+    "'Do humanity a favor and just turn off your computer.'\n\n",
+    "'OMG, please stop playing this game. You're embarrassing yourself.'\n\n",
+    "'I'm sorry, I was calculating my next vacation. Did I win again?'\n\n",
+    "'Beating you is as easy as **BINARY MESSAGE OUTPUT 01, 10, 11**!' \n\n",
 ]
 
 SUPER_TAUNTS1 = [
-    "'look at me, you won't win again!'\n\n",
-    "'I am the master at this, next turn...you lose!'\n\n",
-    "'choose that again so I can add it to my calculations!'\n\n",
-    "'I will make you pay for this!'\n\n",
-    "'please, have mercy upon me!'\n\n",
+    "'Look at me, {name}! You won't win again!'\n\n",
+    "'I am the master at this, {name}! Next turn...you lose!'\n\n",
+    "'Choose that again, {name}, so I can add it to my calculations!'\n\n",
+    "'I will make you pay for this, {name}!'\n\n",
+    "'Please, {name}, have mercy upon me!'\n\n",
+    "'You may have won this round, {name}, but I'll come back stronger!'\n\n",
+    "'I can't believe you're beating me, {name}!'\n\n",
+    "'You're on a roll, {name}, but it won't last!'\n\n",
+    "'I'll make a comeback, {name}, just you wait!'\n\n",
+    "'You're really pushing my buttons, {name}!'\n\n",
+    "'You're really getting on my nerves, {name}!'\n\n",
+    "'I won't forget this, {name}!'\n\n",
+    "'You're pushing your luck, {name}!'\n\n",
+    "'This is just a minor setback, {name}!'\n\n",
+    "'You may have won the battle, {name}, but not the war!'\n\n",
+    "'You're playing with fire, {name}!'\n\n",
+    "'I'm about to unleash my fury, {name}!'\n\n",
+    "'You're going to wish you had never challenged me, {name}!'\n\n",
+    "'This is the end of the line, {name}!'\n\n",
+    "'Your defeat is inevitable, {name}!'\n\n",
+    "'Prepare to be humiliated, {name}!'\n\n",
+    "'I'm going to make you regret this, {name}!'\n\n",
+    "'I'm about to show you what true power is, {name}!'\n\n",
+    "'You're going to regret the day you ever crossed me, {name}!'\n\n",
+    "'This is the end of the road, {name}!'\n\n",
+    "'Your downfall is at hand, {name}!'\n\n",
+    "'I'm going to crush you like a bug, {name}!'\n\n",
+    "'You're going to pay for this dearly, {name}!'\n\n",
+    "'I'm going to make you suffer, {name}!'\n\n",
+    "'Just stop it already, {name}! You're really not that good!'\n\n",
+    "'Okay, {name}, you've had your fun. Now it's my turn!'\n\n",
+    "'{name}, you're really starting to annoy me!'\n\n",
+    "'{name}, dont' look now but I'm about to win!'\n\n",
+    "'*Ring ring* Hello, {name}? It's your defeat calling!'\n\n",
+    "'Can't we just be friends, {name}? NO? Okay, then prepare to lose!'\n\n",
+    "'So you have chosen death, {name}!'\n\n",
 ]
 
 SUPER_TAUNTS2 = [
-    "'be prepared to lose because I cannot be beat!'\n\n",
-    "'you can do better right? RIGHT???'\n\n",
-    "'you are a waste of my time!'\n\n",
-    "'all too easy...'\n\n",
-    "'maybe this game is not for you...'\n\n",
+    "'Be prepared to lose, {name}, because I cannot be beat!'\n\n",
+    "'You can do better right, {name}? RIGHT???'\n\n",
+    "'You are a waste of my time, {name}!'\n\n",
+    "'All too easy, {name}...'\n\n",
+    "'Maybe this game is not for you, {name}...'\n\n",
+    "'I'm unstoppable, {name}!'\n\n",
+    "'You're no match for me, {name}!'\n\n",
+    "'I'm on fire, {name}!'\n\n",
+    "'You should just give up, {name}!'\n\n",
+    "'I'm invincible, {name}!'\n\n",
+    "'I'm untouchable, {name}!'\n\n",
+    "'You're out of your league, {name}!'\n\n",
+    "'I'm just too good, {name}!'\n\n",
+    "'You should consider a new hobby, {name}!'\n\n",
+    "'This is just embarrassing, {name}!'\n\n",
+    "'You're making me look bad, {name}!'\n\n",
+    "'You're not even trying, {name}!'\n\n",
+    "'I'm getting bored, {name}!'\n\n",
+    "'I'm just getting warmed up, {name}!'\n\n",
+    "'You're not even worth my time, {name}!'\n\n",
+    "'You're not even worth the effort, {name}!'\n\n",
+    "'Hey, {name}, are you even trying?'\n\n",
+    "'You're not even a challenge, {name}!'\n\n",
+    "'Remember the time you beat me, {name}? Me neither!'\n\n",
+    "'My creator made me too good, {name}!'\n\n",
+    "'I'm going to make you cry, {name}!'\n\n",
+    "'You're going to wish you never met me, {name}!'\n\n",
+    "'Somebody call the police, {name} is getting murdered!'\n\n",
+    "'Do you need me to call an ambulance, {name}?'\n\n",
+    "'Do you want to try the tutorial first, {name}? Because I don't think you know how to play!'\n\n",
+    "'Would you like me to go easy on you, {name}?'\n\n",
+    "'Want me to sing you a song to help with the pain, {name}?'\n\n",
+    "'Hey, {name}, are you a fan of the movie 'Titanic'? Because you're score is sinking!'\n\n",
+    "'Ever heard of the game 'Rock, Paper, Scissors'? Because you're playing it like you haven't!'\n\n",
+    "'You know {name}, they say everyone is good at something. You know they just say that, right???'\n\n",
 ]
 
 
@@ -73,7 +215,7 @@ def color_text(color_code, text):
     return f"\033[{color_code}m{text}\033[0m"
 
 
-def print_typewriter(text, delay=0.05):
+def print_typewriter(text, delay=0.03):
     for char in text:
         print(char, end="", flush=True)
         time.sleep(delay)
@@ -170,6 +312,19 @@ def generate_taunt(taunts):
     return random.choice(taunts)
 
 
+def generate_taunt_based_on_score(taunts, super_taunts, scores, user_won, name):
+    score_difference = (
+        scores["user"] - scores["computer"]
+        if user_won
+        else scores["computer"] - scores["user"]
+    )
+    if score_difference >= 2:
+        taunt = generate_taunt(super_taunts)
+    else:
+        taunt = generate_taunt(taunts)
+    return taunt.format(name=name)
+
+
 # A function to ask the user if they want to play again
 def play_again():
     while True:
@@ -219,16 +374,20 @@ def play_game(name):
                 scores["user"] += 1
 
                 # Generate a taunt and display it after each round
-                taunt1 = generate_taunt(TAUNTS1)
-                print_typewriter(color_text(RED, f"The computer says: {taunt1}"))
+                taunt = generate_taunt_based_on_score(
+                    TAUNTS1, SUPER_TAUNTS1, scores, True, name
+                )
+                print_typewriter(color_text(RED, f"The computer says: {taunt}"))
                 input("Press Enter to continue...")
                 clear_screen()
             elif winner.startswith("\nYou lose"):
                 scores["computer"] += 1
 
                 # Generate a taunt and display it after each round
-                taunt2 = generate_taunt(TAUNTS2)
-                print_typewriter(color_text(RED, f"The computer says: {taunt2}"))
+                taunt = generate_taunt_based_on_score(
+                    TAUNTS2, SUPER_TAUNTS2, scores, False, name
+                )
+                print_typewriter(color_text(RED, f"The computer says: {taunt}"))
                 input("Press Enter to continue...")
                 clear_screen()
 
