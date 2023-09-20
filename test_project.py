f90ff1a1bf1cef4a7d3486b2ecdb17c1f7087bcd
unknown <brannongarrett77@gmail.com>
2023-09-20 14:41:01 +0200

    Changed file names to 'project'

--- test_project.py
+++ test_project.py
@@ -1,5 +1,5 @@
 import unittest.mock
-from rock_paper_scissors import (
+from project import (
     get_name,
     validate_name,
     get_difficulty,
@@ -10,14 +10,10 @@ MOVES = ["rock", "paper", "scissors"]
 
 
 def test_get_name():
-    with unittest.mock.patch(
-        "rock_paper_scissors.input", return_value="John"
-    ), unittest.mock.patch(
-        "rock_paper_scissors.validate_name", return_value=True
-    ), unittest.mock.patch(
-        "rock_paper_scissors.clear_screen"
-    ), unittest.mock.patch(
-        "rock_paper_scissors.print_typewriter"
+    with unittest.mock.patch("project.input", return_value="John"), unittest.mock.patch(
+        "project.validate_name", return_value=True
+    ), unittest.mock.patch("project.clear_screen"), unittest.mock.patch(
+        "project.print_typewriter"
     ):
         result = get_name()
         assert result == "John"
@@ -35,19 +31,17 @@ def test_validate_name():
 
 def test_get_difficulty():
     # Test valid difficulty levels
-    with unittest.mock.patch("rock_paper_scissors.input", return_value="easy"):
+    with unittest.mock.patch("project.input", return_value="easy"):
         assert get_difficulty() == "easy"
 
-    with unittest.mock.patch("rock_paper_scissors.input", return_value="medium"):
+    with unittest.mock.patch("project.input", return_value="medium"):
         assert get_difficulty() == "medium"
 
-    with unittest.mock.patch("rock_paper_scissors.input", return_value="hard"):
+    with unittest.mock.patch("project.input", return_value="hard"):
         assert get_difficulty() == "hard"
 
     # Test invalid input handling
-    with unittest.mock.patch(
-        "rock_paper_scissors.input", side_effect=["invalid", "easy"]
-    ):
+    with unittest.mock.patch("project.input", side_effect=["invalid", "easy"]):
         assert get_difficulty() == "easy"
 
 
@@ -58,16 +52,12 @@ def test_generate_random_move_easy():
 
 
 def test_generate_random_move_medium():
-    with unittest.mock.patch(
-        "rock_paper_scissors.generate_medium_move", return_value="paper"
-    ):
+    with unittest.mock.patch("project.generate_medium_move", return_value="paper"):
         result = generate_random_move(MOVES, "medium", [])
         assert result == "paper"
 
 
 def test_generate_random_move_hard():
-    with unittest.mock.patch(
-        "rock_paper_scissors.generate_hard_move", return_value="scissors"
-    ):
+    with unittest.mock.patch("project.generate_hard_move", return_value="scissors"):
         result = generate_random_move(MOVES, "hard", [])
         assert result == "scissors"
